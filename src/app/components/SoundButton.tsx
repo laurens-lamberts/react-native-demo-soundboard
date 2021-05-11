492e4d87fcb63bdead9289bf2842d704a618012c
Laurens Lamberts <laurens.lamberts@weareyou.com>
2021-05-11 22:28:40 +0200

    easier example

--- src/app/components/SoundButton.tsx
+++ src/app/components/SoundButton.tsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import {StyleSheet, TouchableOpacity, useWindowDimensions} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 interface Props {
-  onPress: () => void;
+  sound: any;
   icon: string;
 }
 
-const SoundButton = ({onPress, icon}: Props) => {
-  const {width} = useWindowDimensions();
-  const BUTTON_WIDTH = width * 0.3;
+const SoundButton = ({ icon, sound }: Props) => {
+  const BUTTON_SIZE = 114;
+  const margin = 12;
 
   return (
     <TouchableOpacity
       style={{
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: '#000',
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: "#000",
         shadowRadius: 10,
         shadowOpacity: 0.2,
         elevation: 10,
-        width: BUTTON_WIDTH,
-        height: BUTTON_WIDTH,
-        borderRadius: BUTTON_WIDTH / 2,
-        marginBottom: width * 0.025,
+        width: BUTTON_SIZE,
+        height: BUTTON_SIZE,
+        borderRadius: BUTTON_SIZE / 4,
+        marginLeft: margin,
+        marginBottom: margin,
       }}
-      onPress={onPress}>
-      <Icon name={icon} size={48} color="#900" />
+      onPress={() => {
+        if (!sound) {
+          return;
+        }
+
+        if (sound.isPlaying()) {
+          sound.stop();
+        }
+
+        sound.play((success: boolean) => {
+          if (success) {
+            console.log("successfully finished playing");
+          } else {
+            console.log("playback failed due to audio decoding errors");
+          }
+        });
+      }}
+    >
+      <Icon name={icon} size={BUTTON_SIZE / 2} color="#900" />
     </TouchableOpacity>
   );
 };
--- src/domains/soundboard/SoundBoard.tsx
+++ src/domains/soundboard/SoundBoard.tsx
@@ -1,66 +1,58 @@
-import React from 'react';
-import SoundButton from '../../app/components/SoundButton';
-import {Platform, SafeAreaView, ScrollView, View} from 'react-native';
-import Sound from 'react-native-sound';
+import React from "react";
+import SoundButton from "../../app/components/SoundButton";
+import { Platform, SafeAreaView, ScrollView, View } from "react-native";
+import Sound from "react-native-sound";
 
 const SoundBoard = () => {
-  var sound1 = new Sound(
-    (Platform.OS === 'ios' ? 'assets/' : '') + 'blijven_zitten.mp3',
-    Sound.MAIN_BUNDLE,
-  );
-  var sound2 = new Sound(
-    (Platform.OS === 'ios' ? 'assets/' : '') + 'attention.mp3',
-    Sound.MAIN_BUNDLE,
-  );
+  const BUTTONS = [
+    {
+      icon: "warning",
+      sound: new Sound(
+        (Platform.OS === "ios" ? "assets/" : "") + "blijven_zitten.mp3",
+        Sound.MAIN_BUNDLE
+      ),
+    },
+    {
+      icon: "rocket",
+      sound: new Sound(
+        (Platform.OS === "ios" ? "assets/" : "") + "attention.mp3",
+        Sound.MAIN_BUNDLE
+      ),
+    },
+    {
+      icon: "rocket",
+    },
+    {
+      icon: "rocket",
+    },
+    {
+      icon: "rocket",
+    },
+    {
+      icon: "rocket",
+    },
+    {
+      icon: "rocket",
+    },
+    {
+      icon: "rocket",
+    },
+  ];
 
   return (
-    <View style={{flex: 1, backgroundColor: 'tomato'}}>
-      <SafeAreaView style={{flex: 1}}>
+    <View style={{ flex: 1, backgroundColor: "tomato" }}>
+      <SafeAreaView style={{ flex: 1 }}>
         <ScrollView>
           <View
             style={{
               flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              flexWrap: 'wrap',
-              padding: 12,
-            }}>
-            <SoundButton
-              icon={'rocket'}
-              onPress={() => {
-                if (sound1.isPlaying()) {
-                  sound1.stop();
-                }
-
-                sound1.play(success => {
-                  if (success) {
-                    console.log('successfully finished playing');
-                  } else {
-                    console.log('playback failed due to audio decoding errors');
-                  }
-                });
-              }}
-            />
-            <SoundButton
-              icon={'warning'}
-              onPress={() => {
-                if (sound2.isPlaying()) {
-                  sound2.stop();
-                }
-                sound2.play();
-              }}
-            />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
-            <SoundButton />
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            {BUTTONS.map((b) => (
+              <SoundButton icon={b.icon} sound={b.sound} />
+            ))}
           </View>
         </ScrollView>
       </SafeAreaView>
